966af0432a20759dcc6c43dd90fd4e21ea465f28
Julien Goux <hi@jgoux.dev>
2024-08-09 13:45:16 +0200

    group components

--- apps/postgres-new/components/sidebar.tsx
+++ apps/postgres-new/components/sidebar.tsx
@@ -38,6 +38,8 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { DialogPortal, DialogTrigger } from '@radix-ui/react-dialog'
+import React from 'react'
 
 export default function Sidebar() {
   const { user, signOut, focusRef } = useApp()
@@ -231,99 +233,29 @@ type DatabaseMenuItemProps = {
 }
 
 function DatabaseMenuItem({ database, isActive }: DatabaseMenuItemProps) {
-  const router = useRouter()
-  const { dbManager, user } = useApp()
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
-  const { mutateAsync: deleteDatabase } = useDatabaseDeleteMutation()
-  const { mutateAsync: updateDatabase } = useDatabaseUpdateMutation()
+  const {
+    dropdownTriggerProps,
+    dropdownMenuProps,
+    dropdownMenuContentProps,
+    dialogProps,
+    dropdownMenuItemProps,
+    setDropdownOpen,
+  } = useDropdownMenuWithDialogs()
 
   const [isRenaming, setIsRenaming] = useState(false)
-  const [isDeployDialogOpen, setIsDeployDialogOpen] = useState(false)
-
-  const { data: isOnDeployWaitlist } = useIsOnDeployWaitlistQuery()
-  const { mutateAsync: joinDeployWaitlist } = useDeployWaitlistCreateMutation()
 
   return (
-    <>
-      <Dialog
-        open={isDeployDialogOpen}
-        onOpenChange={(open) => {
-          setIsDeployDialogOpen(open)
-        }}
-      >
-        <DialogContent className="max-w-2xl">
-          <DialogHeader>
-            <DialogTitle>Deployments are in Private Alpha</DialogTitle>
-            <div className="py-2 border-b" />
-          </DialogHeader>
-          <h2 className="font-medium">What are deployments?</h2>
-          <p>
-            Deploy your database to a serverless PGlite instance so that it can be accessed outside
-            the browser using any Postgres client:
-          </p>
-          <CodeBlock
-            className="language-curl bg-neutral-800"
-            language="curl"
-            hideLineNumbers
-            theme="dark"
-          >
-            {`psql "postgres://postgres:<password>@<your-unique-server>/postgres"`}
-          </CodeBlock>
-          <div className="flex justify-center items-center mt-3">
-            <AnimatePresence initial={false}>
-              {!isOnDeployWaitlist ? (
-                <button
-                  className="px-4 py-3 bg-black text-white rounded-md"
-                  onClick={async () => {
-                    await joinDeployWaitlist()
-                  }}
-                >
-                  Join Private Alpha
-                </button>
-              ) : (
-                <m.div
-                  className="px-4 py-3 border-2 rounded-md text-center border-dashed"
-                  variants={{
-                    hidden: { scale: 0 },
-                    show: { scale: 1 },
-                  }}
-                  initial="hidden"
-                  animate="show"
-                >
-                  <h3 className="font-medium mb-2">🎉 You&apos;re on the waitlist!</h3>
-                  <p>We&apos;ll send you an email when you have access to deploy.</p>
-                </m.div>
-              )}
-            </AnimatePresence>
-          </div>
-        </DialogContent>
-      </Dialog>
-
-      <Link
-        data-active={isActive || isPopoverOpen}
-        className={cn(
-          'group text-sm w-full relative justify-start bg-card hover:bg-muted/50 flex gap-2 px-3 h-10 items-center rounded-md overflow-hidden data-[active=true]:bg-accent transition'
-        )}
-        href={`/db/${database.id}`}
-      >
-        <span className="text-nowrap grow truncate">{database.name ?? 'My database'}</span>
-        <DropdownMenu
-          modal={false}
-          onOpenChange={(open) => {
-            setIsPopoverOpen(open)
-            if (!open) {
-              setIsRenaming(false)
-            }
-          }}
-          open={isPopoverOpen}
-        >
-          <DropdownMenuTrigger
-            className="group/trigger outline-none"
-            onClick={(e) => {
-              e.preventDefault()
-              setIsPopoverOpen(true)
-            }}
-          >
+    <Link
+      data-active={isActive || dropdownMenuProps.open}
+      className={cn(
+        'group text-sm w-full relative justify-start bg-card hover:bg-muted/50 flex gap-2 px-3 h-10 items-center rounded-md overflow-hidden data-[active=true]:bg-accent transition'
+      )}
+      href={`/db/${database.id}`}
+    >
+      <span className="text-nowrap grow truncate">{database.name ?? 'My database'}</span>
+      <DropdownMenu modal={false} {...dropdownMenuProps}>
+        <DropdownMenuTrigger className="group/trigger outline-none" asChild>
+          <button {...dropdownTriggerProps}>
             <MoreVertical
               size={16}
               className={cn(
@@ -334,140 +266,300 @@ function DatabaseMenuItem({ database, isActive }: DatabaseMenuItemProps) {
                 'transition'
               )}
             />
-          </DropdownMenuTrigger>
+          </button>
+        </DropdownMenuTrigger>
 
-          <DropdownMenuContent side="right" align="start">
-            {isRenaming ? (
-              <form
-                className="w-72"
-                onSubmit={async (e) => {
+        <DropdownMenuContent side="right" align="start" {...dropdownMenuContentProps}>
+          {isRenaming ? (
+            <RenameDatabaseForm
+              database={database}
+              onSuccess={() => {
+                setDropdownOpen(false)
+                setIsRenaming(false)
+              }}
+            />
+          ) : (
+            <div className="flex flex-col items-stretch w-32">
+              <RenameMenuItem
+                database={database}
+                onSelect={(e) => {
                   e.preventDefault()
-
-                  if (e.target instanceof HTMLFormElement) {
-                    const formData = new FormData(e.target)
-                    const name = formData.get('name')
-
-                    if (typeof name === 'string') {
-                      await updateDatabase({ ...database, name })
-                    }
-                  }
-
-                  setIsPopoverOpen(false)
-                  setIsRenaming(false)
+                  setIsRenaming(true)
                 }}
-              >
-                <input
-                  name="name"
-                  className="flex-grow w-full p-2 outline-none text-base bg-inherit placeholder:text-neutral-400"
-                  placeholder={`Rename ${database.name}`}
-                  defaultValue={database.name ?? undefined}
-                  autoComplete="off"
-                  autoFocus
-                />
-              </form>
-            ) : (
-              <div className="flex flex-col items-stretch w-32">
-                <DropdownMenuItem
-                  className="gap-3"
-                  onSelect={async (e) => {
-                    e.preventDefault()
-                    setIsRenaming(true)
-                  }}
-                >
-                  <Pencil
-                    size={16}
-                    strokeWidth={2}
-                    className="flex-shrink-0 text-muted-foreground"
-                  />
-                  <span>Rename</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  className="gap-3"
-                  onSelect={async (e) => {
-                    e.preventDefault()
-
-                    if (!dbManager) {
-                      throw new Error('dbManager is not available')
-                    }
-
-                    const db = await dbManager.getDbInstance(database.id)
-                    const dumpBlob = await db.dumpDataDir()
-
-                    const fileName = `${titleToKebabCase(database.name ?? 'My Database')}-${Date.now()}`
-                    const file = new File([dumpBlob], fileName, { type: dumpBlob.type })
-
-                    downloadFile(file)
-                  }}
-                >
-                  <Download size={16} strokeWidth={2} className="flex-shrink-0" />
-
-                  <span>Download</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  className="bg-inherit justify-start hover:bg-neutral-200 flex gap-3"
-                  onClick={async (e) => {
-                    e.preventDefault()
-
-                    if (!dbManager) {
-                      throw new Error('No dbManager')
-                    }
-
-                    const db = await dbManager.getDbInstance(database.id)
-                    const dump = await db.dumpDataDir()
-                    const response = await fetch(`/api/databases/${database.id}/upload`, {
-                      method: 'POST',
-                      body: dump,
-                    })
-
-                    type Result =
-                      | { success: true; data: { serverName: string } }
-                      | { success: false; error: string }
-
-                    const result: Result = await response.json()
-
-                    console.log(result)
-                    // if (result.success) {
-                    //   setPublishServerName(result.data.serverName)
-                    // } else {
-                    //   setPublishErrorMessage(result.error)
-                    // }
-                    // setIsDeployDialogOpen(true)
-                    // setIsPopoverOpen(false)
-                  }}
-                  disabled={user === undefined}
-                >
-                  <Upload
-                    size={16}
-                    strokeWidth={2}
-                    className="flex-shrink-0 text-muted-foreground"
-                  />
-                  <span>Publish</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  className="gap-3"
-                  onSelect={async (e) => {
-                    e.preventDefault()
-                    setIsPopoverOpen(false)
-                    await deleteDatabase({ id: database.id })
-
-                    if (isActive) {
-                      router.push('/')
-                    }
-                  }}
-                >
-                  <Trash2
-                    size={16}
-                    strokeWidth={2}
-                    className="flex-shrink-0 text-muted-foreground"
-                  />
-                  <span>Delete</span>
-                </DropdownMenuItem>
-              </div>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </Link>
-    </>
+              />
+              <DownloadMenuItem database={database} />
+              <DeployDialog {...dialogProps}>
+                <DeployMenuItem {...dropdownMenuItemProps} database={database} />
+              </DeployDialog>
+              <DropdownMenuSeparator />
+              <DeleteMenuItem database={database} isActive={isActive} />
+            </div>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Link>
   )
+}
+
+function DeleteMenuItem(props: { database: Database; isActive: boolean }) {
+  const router = useRouter()
+  const { mutateAsync: deleteDatabase } = useDatabaseDeleteMutation()
+
+  async function handleMenuItemSelect(e: Event) {
+    await deleteDatabase({ id: props.database.id })
+
+    if (props.isActive) {
+      router.push('/')
+    }
+  }
+
+  return (
+    <DropdownMenuItem className="gap-3" onSelect={handleMenuItemSelect}>
+      <Trash2 size={16} strokeWidth={2} className="flex-shrink-0 text-muted-foreground" />
+      <span>Delete</span>
+    </DropdownMenuItem>
+  )
+}
+
+function RenameDatabaseForm(props: { database: Database; onSuccess: () => void }) {
+  const { mutateAsync: updateDatabase } = useDatabaseUpdateMutation()
+
+  return (
+    <form
+      className="w-72"
+      onSubmit={async (e) => {
+        e.preventDefault()
+        if (e.target instanceof HTMLFormElement) {
+          const formData = new FormData(e.target)
+          const name = formData.get('name')
+
+          if (typeof name === 'string') {
+            await updateDatabase({ ...props.database, name })
+          }
+        }
+        props.onSuccess()
+      }}
+    >
+      <input
+        name="name"
+        className="flex-grow w-full p-2 outline-none text-base bg-inherit placeholder:text-neutral-400"
+        placeholder={`Rename ${props.database.name}`}
+        defaultValue={props.database.name ?? undefined}
+        autoComplete="off"
+        autoFocus
+      />
+    </form>
+  )
+}
+
+function RenameMenuItem(props: { database: Database; onSelect: (e: Event) => void }) {
+  return (
+    <DropdownMenuItem className="gap-3" onSelect={props.onSelect}>
+      <Pencil size={16} strokeWidth={2} className="flex-shrink-0 text-muted-foreground" />
+      <span>Rename</span>
+    </DropdownMenuItem>
+  )
+}
+
+function DownloadMenuItem({ database }: { database: Database }) {
+  const { dbManager } = useApp()
+
+  async function handleMenuItemSelect(e: Event) {
+    if (!dbManager) {
+      throw new Error('dbManager is not available')
+    }
+
+    const db = await dbManager.getDbInstance(database.id)
+    const dumpBlob = await db.dumpDataDir()
+
+    const fileName = `${titleToKebabCase(database.name ?? 'My Database')}-${Date.now()}`
+    const file = new File([dumpBlob], fileName, { type: dumpBlob.type })
+
+    downloadFile(file)
+  }
+
+  return (
+    <DropdownMenuItem className="gap-3" onSelect={handleMenuItemSelect}>
+      <Download size={16} strokeWidth={2} className="flex-shrink-0" />
+
+      <span>Download</span>
+    </DropdownMenuItem>
+  )
+}
+
+const DeployDialog = React.forwardRef(
+  (
+    props: {
+      children: React.ReactNode
+      onOpenChange: (open: boolean) => void
+    },
+    ref: React.Ref<HTMLButtonElement>
+  ) => {
+    const { data: isOnDeployWaitlist } = useIsOnDeployWaitlistQuery()
+    const { mutateAsync: joinDeployWaitlist } = useDeployWaitlistCreateMutation()
+
+    return (
+      <Dialog onOpenChange={props.onOpenChange}>
+        <DialogTrigger ref={ref} asChild>
+          {props.children}
+        </DialogTrigger>
+        <DialogPortal>
+          <DialogContent className="max-w-2xl">
+            <DialogHeader>
+              <DialogTitle>Deployments are in Private Alpha</DialogTitle>
+              <div className="py-2 border-b" />
+            </DialogHeader>
+            <h2 className="font-medium">What are deployments?</h2>
+            <p>
+              Deploy your database to a serverless PGlite instance so that it can be accessed
+              outside the browser using any Postgres client:
+            </p>
+            <CodeBlock
+              className="language-curl bg-neutral-800"
+              language="curl"
+              hideLineNumbers
+              theme="dark"
+            >
+              {`psql "postgres://postgres:<password>@<your-unique-server>/postgres"`}
+            </CodeBlock>
+            <div className="flex justify-center items-center mt-3">
+              <AnimatePresence initial={false}>
+                {!isOnDeployWaitlist ? (
+                  <button
+                    className="px-4 py-3 bg-black text-white rounded-md"
+                    onClick={async () => {
+                      await joinDeployWaitlist()
+                    }}
+                  >
+                    Join Private Alpha
+                  </button>
+                ) : (
+                  <m.div
+                    className="px-4 py-3 border-2 rounded-md text-center border-dashed"
+                    variants={{
+                      hidden: { scale: 0 },
+                      show: { scale: 1 },
+                    }}
+                    initial="hidden"
+                    animate="show"
+                  >
+                    <h3 className="font-medium mb-2">🎉 You&apos;re on the waitlist!</h3>
+                    <p>We&apos;ll send you an email when you have access to deploy.</p>
+                  </m.div>
+                )}
+              </AnimatePresence>
+            </div>
+          </DialogContent>
+        </DialogPortal>
+      </Dialog>
+    )
+  }
+)
+
+const DeployMenuItem = React.forwardRef<
+  HTMLDivElement,
+  { database: Database; onSelect: (event: Event) => void }
+>((props, ref) => {
+  const { dbManager, user } = useApp()
+  const { database, ...menuItemProps } = props
+
+  async function handleDeployClick() {
+    if (!dbManager) {
+      throw new Error('No dbManager')
+    }
+    // const db = await dbManager.getDbInstance(props.database.id)
+    // const dump = await db.dumpDataDir()
+    // const response = await fetch(`/api/databases/${props.database.id}/upload`, {
+    //   method: 'POST',
+    //   body: dump,
+    // })
+    // type Result =
+    //   | { success: true; data: { serverName: string } }
+    //   | { success: false; error: string }
+    // const result: Result = await response.json()
+    // console.log(result)
+    // if (result.success) {
+    //   setPublishServerName(result.data.serverName)
+    // } else {
+    //   setPublishErrorMessage(result.error)
+    // }
+    // setIsDeployDialogOpen(true)
+    // setIsPopoverOpen(false)
+  }
+
+  return (
+    <DropdownMenuItem
+      className="bg-inherit justify-start hover:bg-neutral-200 flex gap-3"
+      disabled={user === undefined}
+      ref={ref}
+      {...menuItemProps}
+    >
+      <Upload size={16} strokeWidth={2} className="flex-shrink-0 text-muted-foreground" />
+      <span>Deploy</span>
+    </DropdownMenuItem>
+  )
+})
+
+/**
+ * A hook containing the state to control a dropdown menu containing dialogs
+ *
+ * @see https://github.com/radix-ui/primitives/issues/1836#issuecomment-1556688048
+ */
+function useDropdownMenuWithDialogs() {
+  const [dropdownOpen, setDropdownOpen] = React.useState(false)
+  const [hasOpenDialog, setHasOpenDialog] = React.useState(false)
+  const dropdownTriggerRef = React.useRef<HTMLButtonElement | null>(null)
+  const focusRef = React.useRef<HTMLButtonElement | null>(null)
+
+  function handleDialogItemSelect() {
+    focusRef.current = dropdownTriggerRef.current
+  }
+
+  function handleDialogOpenChange(open: boolean) {
+    setHasOpenDialog(open)
+    if (open === false) {
+      setDropdownOpen(false)
+    }
+  }
+
+  const dropdownTriggerProps = {
+    ref: dropdownTriggerRef,
+  }
+
+  const dropdownMenuProps = {
+    open: dropdownOpen,
+    onOpenChange: setDropdownOpen,
+  }
+
+  const dialogProps = {
+    onOpenChange: handleDialogOpenChange,
+  }
+
+  const dropdownMenuContentProps = {
+    hidden: hasOpenDialog,
+    onCloseAutoFocus: (event: Event) => {
+      if (focusRef.current) {
+        focusRef.current.focus()
+        focusRef.current = null
+        event.preventDefault()
+      }
+    },
+  }
+
+  const dropdownMenuItemProps = {
+    onSelect: (event: Event) => {
+      event.preventDefault()
+      handleDialogItemSelect()
+    },
+  }
+
+  return {
+    dropdownOpen,
+    setDropdownOpen,
+    dropdownTriggerProps,
+    dropdownMenuProps,
+    dropdownMenuContentProps,
+    dialogProps,
+    dropdownMenuItemProps,
+  }
 }
