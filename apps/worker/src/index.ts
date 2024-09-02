6f14056f6619116996eea5d00f99ab123f152b3e
Julien Goux <hi@jgoux.dev>
2024-09-02 20:56:03 +0200

    don't use pg-gateway for now

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,7 +1,7 @@
 import net from 'node:net'
 import { PGlite, type PGliteInterface } from '@electric-sql/pglite'
 import { vector } from '@electric-sql/pglite/vector'
-import { PostgresConnection } from 'pg-gateway'
+// import { PostgresConnection } from 'pg-gateway'
 import { s3GetObject } from './s3-get-object.ts'
 
 const server = net.createServer()
@@ -10,56 +10,84 @@ server.on('connection', async (socket) => {
   console.log('new connection')
   // the first message contains the databaseId
   const databaseId = await new Promise<string>((res) =>
-    socket.once('data', (data) => {
-      console.log('received databaseId from proxy', data)
-      res(data.toString('utf-8'))
-    })
+    socket.once('data', (data) => res(data.toString('utf-8')))
   )
 
   console.log('databaseId', databaseId)
 
   let database: PGliteInterface | undefined
 
-  const connection = new PostgresConnection(socket, {
-    async onMessage(data) {
-      if (!database) {
-        console.time('download pgdata')
-        const response = await s3GetObject({
-          bucket: process.env.AWS_S3_BUCKET!,
-          endpoint: process.env.AWS_ENDPOINT_URL_S3!,
-          region: process.env.AWS_REGION!,
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-          key: `dbs/${databaseId}.tar.gz`,
-        })
-        const pgdata = new Blob([await response.arrayBuffer()], { type: 'application/gzip' })
-        console.timeEnd('download tarball')
+  // const connection = new PostgresConnection(socket, {
+  //   async onMessage(data) {
+  //     if (!database) {
+  //       console.time('download pgdata')
+  //       const response = await s3GetObject({
+  //         bucket: process.env.AWS_S3_BUCKET!,
+  //         endpoint: process.env.AWS_ENDPOINT_URL_S3!,
+  //         region: process.env.AWS_REGION!,
+  //         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+  //         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  //         key: `dbs/${databaseId}.tar.gz`,
+  //       })
+  //       const pgdata = new Blob([await response.arrayBuffer()], { type: 'application/gzip' })
+  //       console.timeEnd('download tarball')
 
-        console.time('init database')
-        database = await PGlite.create({
-          loadDataDir: pgdata,
-          extensions: {
-            vector,
-          },
-        })
-        console.timeEnd('init database')
-      }
+  //       console.time('init database')
+  //       database = await PGlite.create({
+  //         loadDataDir: pgdata,
+  //         extensions: {
+  //           vector,
+  //         },
+  //       })
+  //       console.timeEnd('init database')
+  //     }
 
-      const response = await database.execProtocolRaw(data)
-      connection.sendData(response)
+  //     const response = await database.execProtocolRaw(data)
+  //     connection.sendData(response)
 
-      return true
-    },
-  })
+  //     return true
+  //   },
+  // })
 
   // send ack to proxy
-  console.log('sending ack to proxy')
-  socket.write('ACK', 'utf-8')
+
+  socket.on('data', async (data) => {
+    console.log('data:', data.toString('hex'))
+
+    if (!database) {
+      console.time('download pgdata')
+      const response = await s3GetObject({
+        bucket: process.env.AWS_S3_BUCKET!,
+        endpoint: process.env.AWS_ENDPOINT_URL_S3!,
+        region: process.env.AWS_REGION!,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        key: `dbs/${databaseId}.tar.gz`,
+      })
+      const pgdata = new Blob([await response.arrayBuffer()], { type: 'application/gzip' })
+      console.timeEnd('download tarball')
+
+      console.time('init database')
+      database = await PGlite.create({
+        loadDataDir: pgdata,
+        extensions: {
+          vector,
+        },
+      })
+      console.timeEnd('init database')
+    }
+
+    const response = await database.execProtocolRaw(data)
+    socket.write(response)
+  })
 
   socket.on('close', async () => {
     await database?.close()
     database = undefined
   })
+
+  console.log('sending ack to proxy')
+  socket.write('ACK', 'utf-8')
 })
 
 server.listen(5432, () => {
