13c3a8ebf3d5fb14cae4b2025ffbc83f0610f331
Julien Goux <hi@jgoux.dev>
2024-09-03 23:12:00 +0200

    comment

--- apps/proxy/src/utils/get-worker.ts
+++ apps/proxy/src/utils/get-worker.ts
@@ -27,6 +27,10 @@ async function getWorkers(): Promise<Worker[]> {
   }))
 }
 
+// TODO: have a better way of putting a lock when getting a worker
+// we currently rely on the fly.io api to keep track of the state of the machines
+// but under heavy load it's slow as API requests are queued
+// and we are putting pressure on fly.io api and could get rate limited
 async function getWorker(): Promise<Worker> {
   const MAX_WAIT_TIME = 10_000 // 10 seconds
   const CHECK_INTERVAL = 500 // 500ms
