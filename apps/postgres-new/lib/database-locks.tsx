6fc4f9559afbcc45e6f93bffcbe4eb9d1d36b920
Julien Goux <hi@jgoux.dev>
2024-11-13 18:32:31 +0100

    logs

--- apps/postgres-new/lib/database-locks.tsx
+++ apps/postgres-new/lib/database-locks.tsx
@@ -4,6 +4,14 @@ const DATABASE_LOCK_PREFIX = 'database-build-db-lock:'
 
 async function getLocks() {
   const result = await navigator.locks.query()
+  console.log(
+    'locks held',
+    result.held?.filter((lock) => lock.name?.startsWith(DATABASE_LOCK_PREFIX))
+  )
+  console.log(
+    'locks pending',
+    result.pending?.filter((lock) => lock.name?.startsWith(DATABASE_LOCK_PREFIX))
+  )
   if (result.held) {
     return result.held
       .map((lock) => lock.name)
@@ -15,7 +23,7 @@ async function getLocks() {
 
 type DatabaseLocksContextState = {
   locks: string[]
-  activeLock: { databaseId: string; release: () => void } | null
+  activeLock: { databaseId: string; release: () => void; abortController: AbortController } | null
 }
 
 const DatabaseLocksContext = createContext<{
@@ -49,12 +57,28 @@ export function DatabaseLocksProvider({ children }: { children: React.ReactNode 
   const acquireLock = useCallback(
     async (databaseId: string) => {
       let resolve: () => void
+      const abortController = new AbortController()
       const lockPromise = new Promise<void>((res) => {
         resolve = res
+      }).then(async () => {
+        console.log(`${databaseId} lock released`)
+        const result = await navigator.locks.query()
+        console.log(
+          'locks held',
+          result.held?.filter((lock) => lock.name?.startsWith(DATABASE_LOCK_PREFIX))
+        )
+        console.log(
+          'locks pending',
+          result.pending?.filter((lock) => lock.name?.startsWith(DATABASE_LOCK_PREFIX))
+        )
       })
-      navigator.locks.request(`${DATABASE_LOCK_PREFIX}${databaseId}`, () => lockPromise)
+      navigator.locks.request(
+        `${DATABASE_LOCK_PREFIX}${databaseId}`,
+        { signal: abortController.signal },
+        () => lockPromise
+      )
       setState((previousState) => ({
-        activeLock: { databaseId, release: resolve },
+        activeLock: { databaseId, release: resolve, abortController },
         locks: [...previousState.locks, databaseId],
       }))
     },
@@ -64,6 +88,7 @@ export function DatabaseLocksProvider({ children }: { children: React.ReactNode 
   const releaseLock = useCallback(
     async (databaseId: string) => {
       state.activeLock?.release()
+      state.activeLock?.abortController.abort('unmount')
       setState((previousState) => ({
         activeLock: null,
         locks: previousState.locks.filter((id) => id !== databaseId),
