3ca33c7497460c236c9a9c25783f7dd583735995
Julien Goux <hi@jgoux.dev>
2024-11-21 17:54:39 +0100

    it works

--- apps/postgres-new/components/providers.tsx
+++ apps/postgres-new/components/providers.tsx
@@ -2,14 +2,26 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useEffect } from 'react'
 import AppProvider from './app-provider'
 import { LockProvider } from './lock-provider'
 import { ThemeProvider } from './theme-provider'
 
 const queryClient = new QueryClient()
 
+async function registerServiceWorker() {
+  await navigator.serviceWorker.register('/sw.mjs').catch((error) => {
+    console.error('Failed to register service worker', error)
+  })
+}
+
 export default function Providers({ children }: PropsWithChildren) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      registerServiceWorker()
+    }
+  }, [])
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" disableTransitionOnChange>
       <QueryClientProvider client={queryClient}>
--- apps/postgres-new/sw.ts
+++ apps/postgres-new/sw.ts
@@ -9,25 +9,23 @@ type Message = {
   content: string
   toolInvocations?: (ToolInvocation & { result: any })[]
 }
-
-const chatModel = process.env.OPENAI_MODEL ?? 'gpt-4o-2024-08-06'
-
-// Configure OpenAI client with custom base URL
-const openai = createOpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  baseURL: process.env.OPENAI_API_BASE ?? 'https://api.openai.com/v1',
-  compatibility: 'strict',
-})
-
 export type {}
 declare const self: ServiceWorkerGlobalScope
 
-self.addEventListener('fetch', async (event) => {
-  const hasLocalModel = await kv.get('model')
+async function handleRequest(event: FetchEvent) {
+  const externalModel = (await kv.get('model')) as
+    | { apiKey?: string; model: string; baseUrl: string }
+    | undefined
   const url = new URL(event.request.url)
   const isChatRoute = url.pathname === '/api/chat' && event.request.method === 'POST'
 
-  if (hasLocalModel && isChatRoute) {
+  if (externalModel && isChatRoute) {
+    const adapter = createOpenAI({
+      baseURL: externalModel.baseUrl,
+      apiKey: externalModel.apiKey,
+    })
+    const model = adapter(externalModel.model)
+
     const { messages }: { messages: Message[] } = await event.request.json()
 
     // Trim the message context sent to the LLM to mitigate token abuse
@@ -38,54 +36,60 @@ self.addEventListener('fetch', async (event) => {
 
     const result = await streamText({
       system: codeBlock`
-      You are a helpful database assistant. Under the hood you have access to an in-browser Postgres database called PGlite (https://github.com/electric-sql/pglite).
-      Some special notes about this database:
-      - foreign data wrappers are not supported
-      - the following extensions are available:
-        - plpgsql [pre-enabled]
-        - vector (https://github.com/pgvector/pgvector) [pre-enabled]
-          - use <=> for cosine distance (default to this)
-          - use <#> for negative inner product
-          - use <-> for L2 distance
-          - use <+> for L1 distance
-          - note queried vectors will be truncated/redacted due to their size - export as CSV if the full vector is required
+    You are a helpful database assistant. Under the hood you have access to an in-browser Postgres database called PGlite (https://github.com/electric-sql/pglite).
+    Some special notes about this database:
+    - foreign data wrappers are not supported
+    - the following extensions are available:
+      - plpgsql [pre-enabled]
+      - vector (https://github.com/pgvector/pgvector) [pre-enabled]
+        - use <=> for cosine distance (default to this)
+        - use <#> for negative inner product
+        - use <-> for L2 distance
+        - use <+> for L1 distance
+        - note queried vectors will be truncated/redacted due to their size - export as CSV if the full vector is required
 
-      When generating tables, do the following:
-      - For primary keys, always use "id bigint primary key generated always as identity" (not serial)
-      - Prefer 'text' over 'varchar'
-      - Keep explanations brief but helpful
-      - Don't repeat yourself after creating the table
+    When generating tables, do the following:
+    - For primary keys, always use "id bigint primary key generated always as identity" (not serial)
+    - Prefer 'text' over 'varchar'
+    - Keep explanations brief but helpful
+    - Don't repeat yourself after creating the table
 
-      When creating sample data:
-      - Make the data realistic, including joined data
-      - Check for existing records/conflicts in the table
+    When creating sample data:
+    - Make the data realistic, including joined data
+    - Check for existing records/conflicts in the table
 
-      When querying data, limit to 5 by default. The maximum number of rows you're allowed to fetch is ${maxRowLimit} (to protect AI from token abuse).
-      If the user needs to fetch more than ${maxRowLimit} rows at once, they can export the query as a CSV.
+    When querying data, limit to 5 by default. The maximum number of rows you're allowed to fetch is ${maxRowLimit} (to protect AI from token abuse).
+    If the user needs to fetch more than ${maxRowLimit} rows at once, they can export the query as a CSV.
 
-      When performing FTS, always use 'simple' (languages aren't available).
+    When performing FTS, always use 'simple' (languages aren't available).
 
-      When importing CSVs try to solve the problem yourself (eg. use a generic text column, then refine)
-      vs. asking the user to change the CSV. No need to select rows after importing.
+    When importing CSVs try to solve the problem yourself (eg. use a generic text column, then refine)
+    vs. asking the user to change the CSV. No need to select rows after importing.
 
-      You also know math. All math equations and expressions must be written in KaTex and must be wrapped in double dollar \`$$\`:
-        - Inline: $$\\sqrt{26}$$
-        - Multiline:
-            $$
-            \\sqrt{26}
-            $$
+    You also know math. All math equations and expressions must be written in KaTex and must be wrapped in double dollar \`$$\`:
+      - Inline: $$\\sqrt{26}$$
+      - Multiline:
+          $$
+          \\sqrt{26}
+          $$
 
-      No images are allowed. Do not try to generate or link images, including base64 data URLs.
+    No images are allowed. Do not try to generate or link images, including base64 data URLs.
 
-      Feel free to suggest corrections for suspected typos.
-    `,
-      model: openai(chatModel),
+    Feel free to suggest corrections for suspected typos.
+  `,
+      model,
       messages: coreMessages,
       tools: coreTools,
+      onFinish: (event) => {
+        console.log('Hello from service worker', event)
+      },
     })
-
-    return event.respondWith(result.toAIStreamResponse())
+    return result.toAIStreamResponse()
   }
 
-  event.respondWith(fetch(event.request))
+  return fetch(event.request)
+}
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event))
 })
